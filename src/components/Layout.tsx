f1da2bc9bcfb41261415c217501392f80debb496
matthew-clarke10 <matthewkclarke10@gmail.com>
2024-07-06 22:19:04 +1000

    Changed layout to a nav element.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,7 +21,7 @@ const Layout: React.FC<LayoutProps> = ({ toggleMusicEnabled, musicEnabled }) => 
             'nonQuiz'
   return (
     <>
-      <div className="flex justify-between items-center h-header-small md:h-header bg-blue-500">
+      <nav className="flex justify-between items-center h-header-small md:h-header bg-blue-500">
         {quizPage && (
           <Link to="/" className="flex justify-center items-center h-full px-1 w-80 md:w-96 font-bold text-center border-4 bg-blue-600 hover:bg-blue-700 border-blue-700 hover:border-blue-800 text-base sm:text-xl md:text-3xl">Home</Link>
         )}
@@ -29,7 +29,7 @@ const Layout: React.FC<LayoutProps> = ({ toggleMusicEnabled, musicEnabled }) => 
         {quizPage && (
           <button onClick={toggleMusicEnabled} className={`h-full px-1 w-80 md:w-96 font-bold whitespace-nowrap text-base sm:text-xl md:text-3xl border-4 ${musicEnabled ? 'bg-green-500 hover:bg-green-600 border-green-700' : 'bg-red-500 hover:bg-red-600 border-red-700'}`}>{musicEnabled ? 'Music Enabled' : 'Music Disabled'}</button>
         )}
-      </div>
+      </nav>
       <main className="bg-gray-100 p-4 h-body-large md:h-body">
         <Outlet />
       </main>
